b92c5423a607f59c06915cdcde3b2f40d800d104
alanasilva88 <alanasilva.as@gmail.com>
2024-07-10 14:49:26 -0300

    Aula novo projeto + bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import Menu from "./components/Menu";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Cadastro from "./pages/Cadastro";
+import Ajuda from "./pages/Ajuda";
+import NotFound from "./pages/NotFound";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// BrowserRouter = componente essecial para conduzir o roteamento do navegador.
+// Router = indicamos a rota (path) e o elemento que será exibido na tela.
+// Poderia colocar o Rodapé abaixo do Routes, como um rodapé fixo, visto que logo acima está o Menu onde fica a navbar
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Menu />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/ajuda" element={<Ajuda />} />
+          <Route path="*" element={<NotFound />} />
+          
+
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Ajuda.jsx
@@ -0,0 +1,32 @@
+import { Container, Accordion, Button, Alert } from "react-bootstrap";
+
+
+function Ajuda() {
+    return (
+        <main>
+            <Container>
+                <h1>Ajuda</h1>
+                <hr />
+                <Accordion defaultActiveKey="0">
+                    <Accordion.Item eventKey="0">
+                        <Accordion.Header>Sobre nosso sistema</Accordion.Header>
+                            <Accordion.Body>
+                            MyTask é um sistema para gerenciamento de tarefas pessoas. Nessa aplicação você pode gerenciar todos os seus afazeres.
+                            </Accordion.Body>
+                    </Accordion.Item>
+                    <Accordion.Item eventKey="1">
+                        <Accordion.Header>Sobre a empresa</Accordion.Header>
+                            <Accordion.Body>
+                            Essa empresa envolve soluções focadas em produtividade e bem-estar.
+                            </Accordion.Body>
+                    </Accordion.Item>
+                </Accordion>
+                <Button className="mt-1 me-1">Entre em contato</Button>
+                <Button className="mt-1 me-1" variant="outline-danger">Denunciar</Button>
+                <Alert className="mt-1" variant="warning">Atenção! Estamos em manutenção!</Alert>
+            </Container>
+        </main >
+    );
+}
+
+export default Ajuda;
